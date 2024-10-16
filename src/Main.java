469816aef4d280ca1ca2c8212deb6d04d90ad245
JaxonZaleski <zaleskjn@mail.uc.edu>
2024-10-16 14:33:03 -0400

    Initial commit, added task 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Count up 1, 0-30");
+        for (int count = 0; count <= 30; count++) {
+            System.out.print(count + " ");
+        }
+        System.out.println("Count down 1, 30-0");
+
+    }
+}
